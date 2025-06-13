e5464359b2562d568409905b61fe10899fa50605
mughni02 <faizalmughni@student.uir.ac.id>
2025-06-13 22:22:49 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-// main.js - Entry point untuk aplikasi Vue.js
 import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 
-// Buat dan mount aplikasi Vue
 createApp(App).mount('#app')
